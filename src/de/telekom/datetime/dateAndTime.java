b77888aae77777a99b4c10e28225868b98670a19
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-04-22 20:45:29 +0200

    first commit

---
+++ src/de/telekom/datetime/dateAndTime.java
@@ -0,0 +1,171 @@
+package de.telekom.datetime;
+
+import java.time.DayOfWeek;
+import java.time.Month;
+
+public class dateAndTime {
+
+	public static void main(String[] args) {
+		
+		final DayOfWeek sunday = DayOfWeek.SUNDAY;
+		final Month february = Month.FEBRUARY;
+		
+		final DayOfWeek friday = sunday.plus(5);
+		final Month december = february.plus(10);
+		
+		System.out.println("1) Start: Day & Time: ");
+		System.out.println(friday);
+		System.out.println(december);
+		System.out.println("*=====================================================*");
+		
+		
+		// Class: --2-- MonthDay, YearMonth and Year
+		System.out.println();
+		System.out.println("2) MonthDay, YearMonth and Year: ");
+		//
+		//
+		//
+		//
+		//
+		System.out.println("*=====================================================*");
+		
+		// Class: --3-- Instant
+				System.out.println();
+				System.out.println("3) Instant: ");
+				//
+				//
+				//
+				//
+				//
+				System.out.println("*=====================================================*");
+				
+				
+				// Class: --4-- Duration
+				System.out.println();
+				System.out.println("4) Duration: ");
+				//
+				//
+				//
+				//
+				//
+				System.out.println("*=====================================================*");
+				
+				
+				// Class: --5-- ChronoUnit
+				System.out.println();
+				System.out.println("5) ChronoUnit: ");
+				//
+				//
+				//
+				//
+				//
+				System.out.println("*=====================================================*");
+				
+				
+				// Class: --6-- LocalDate, LocalTime and LocalDateTime
+				System.out.println();
+				System.out.println("6) LocalDate, LocalTime and LocalDateTime: ");
+				//
+				//
+				//
+				//
+				//
+				System.out.println("*=====================================================*");			
+				
+	
+				
+				
+				// Class: --7-- Period
+				System.out.println();
+				System.out.println("7) Period: ");
+				//
+				//
+				//
+				//
+				//
+				System.out.println("*=====================================================*");				
+				
+				
+				
+				
+				
+				
+				
+				// Class: --8-- ZonedDateTime
+				System.out.println();
+				System.out.println("8) ZonedDateTime: ");
+				//
+				//
+				//
+				//
+				//
+				System.out.println("*=====================================================*");				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				// Class: --9-- ZoneId and ZoneOffset
+				System.out.println();
+				System.out.println("9) ZoneId and ZoneOffset: ");
+				//
+				//
+				//
+				//
+				//
+				System.out.println("*=====================================================*");			
+				
+				
+				
+				
+				
+				
+
+				
+				// Class: --10-- Clock
+				System.out.println();
+				System.out.println("10) Clock: ");
+				//
+				//
+				//
+				//
+				//
+				System.out.println("*=====================================================*");			
+				
+				
+				
+				
+							
+				
+
+
+				
+				// Class: --11-- Formatting and Parsing
+				System.out.println();
+				System.out.println("10) Time: Formatting and Parsing: ");
+				//
+				//
+				//
+				//
+				//
+				System.out.println("*========================> E N D <=============================*");			
+				
+				
+				
+				
+							
+				
+				
+				
+				
+		
+		
+	}
+
+}
